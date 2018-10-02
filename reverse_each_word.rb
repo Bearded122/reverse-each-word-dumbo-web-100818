e57d1b3b2911313d80fe08ed265bb1ac41c9fd1a
John Sime <johnc.sime@gmail.com>
2018-10-02 09:04:24 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
-sentence = [sentence1, sentence2]
-
-def reverse_each_word do |sentence|
-  sentence = sentence.reverse
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
